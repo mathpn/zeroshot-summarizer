bd56066436ded4b8b931613258854923178266e3
Matheus Pedroni <pnmatheus@protonmail.com>
2023-06-15 21:56:49 -0300

    use docker-compose-wait to start containers in the right order

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sleep 10
-exec uvicorn app.main:api --host=0.0.0.0 --workers 2
+/wait
+exec uvicorn app.main:api --host=0.0.0.0 --workers 2 --reload
--- consumer/start.sh
+++ consumer/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sleep 10
+/wait
 exec python -m consumer.worker
